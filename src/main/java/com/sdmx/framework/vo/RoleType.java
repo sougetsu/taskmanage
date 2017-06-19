abe1a0be6f44557dcfdd70dc5e82499bfc5669c2
sogetsu <272083590@qq.com>
2017-06-19 12:28:02 +0800

    update0619

--- src/main/java/com/sdmx/framework/vo/RoleType.java
+++ src/main/java/com/sdmx/framework/vo/RoleType.java
@@ -7,7 +7,9 @@ public enum RoleType {
 	ProductManage("生产部门管理员",4),
 	TestCenterManage("封测中心管理员",5),
 	Default("默认",6),
-	MarketManger("市场人员",7);
+	MarketManger("市场人员",7),
+	TestOutMember("封测外协",8),
+	TestOutManage("封测调度",9);
 	
 	private String label;
 	private int id;
@@ -25,6 +27,12 @@ public enum RoleType {
 	public static RoleType getType(String name) {
 		if(name.equals("市场人员")){
 	        return RoleType.DepartMember;
+	    }
+		if(name.equals("封测外协")){
+	        return RoleType.DepartMember;
+	    }
+		if(name.equals("封测调度")){
+	        return RoleType.DepartManage;
 	    }
         for (RoleType c : RoleType.values()) {  
             if (name.equals(c.getLabel())) {  
